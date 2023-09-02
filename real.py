a9f2bc8ea63671f94ffdbe4f10f94ce90f133d7d
U53re4l <143859294+U53re4l@users.noreply.github.com>
2023-09-02 10:43:33 +0700

    denbeden
    dongabang

---
+++ real.py
@@ -0,0 +1,19 @@
+# apel_benar = True
+# apel_salah = False
+# print(type(apel_benar))
+# print(apel_salah)
+# list_warna_apel = ['hijau','merah','nigga']
+# list_apel1 = ['manis', 2 , 3.0]
+# print(list_warna_apel)
+# print(list_apel1)
+# tuple_jumlah = (2, 5.25, "satu")
+# print(tuple_jumlah)
+# Apel_dictionary = {'nama':'apel','asal':'malang','jumlah':'300'}
+# print(Apel_dictionary)
+# print(34+2)
+# print(34-18)
+# print(2*6)
+# print(12/3)
+b = 6
+a = 14
+print(a // b)
