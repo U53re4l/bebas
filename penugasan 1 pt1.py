04a2b36304457a741de9b4ebd5777416ae43000e
U53re4l <143859294+U53re4l@users.noreply.github.com>
2023-09-09 21:02:15 +0700

    Add files via upload

---
+++ penugasan 1 pt1.py
@@ -0,0 +1,12 @@
+#1. soal 1
+panjang_balok = int(input('panjang balok :'))
+lebar_balok = int(input('lebar balok :'))
+tinggi_balok = int(input('tinggi balok :'))
+
+volume_balok = panjang_balok*lebar_balok*tinggi_balok
+
+print('panjang balok adalah', panjang_balok, 'cm')
+print('lebar balok adalah', lebar_balok, 'cm')
+print('tinggi balok adalah', tinggi_balok, 'cm')
+print('maka volume balok tersebut adalah', volume_balok, 'cm^3')
+
