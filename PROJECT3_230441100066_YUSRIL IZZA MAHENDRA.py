84d7fdf352e776f0cde4f1abd14cabeca9bdb8a3
U53re4l <143859294+U53re4l@users.noreply.github.com>
2023-10-06 08:34:12 +0700

    Delete PROJECT3_230441100066_YUSRIL IZZA MAHENDRA.py

--- PROJECT3_230441100066_YUSRIL IZZA MAHENDRA.py
+++
@@ -1,9 +0,0 @@
-celcius = int(input("masukkan suhu celcius: "))
-
-farenheit = (9/5*celcius)+32
-reamur = 4/5*celcius
-kelvin = celcius+273
-
-print("suhu dalam bentuk farenheit: ", farenheit, "derajat farenheit")
-print("suhu dalam bentuk reamur: ", reamur, "derajat reamur")
-print("suhu dalam bentuk kelvin: ", kelvin,  "derajat kelvin")
